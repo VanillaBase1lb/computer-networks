943e880d1b586f4496a2d9fc592290dd8092851b
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-03-16 18:51:32 +0530

    lab3 completed

--- Lab3/client.py
+++ Lab3/client.py
@@ -1,55 +1,33 @@
-#Client side
 import socket
-import random
 import pickle
 import time
       
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("127.0.0.1",4364))
+sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_client.connect(("127.0.0.1", 4364))
 
-#generate N*N matrix N=4
-mat=[]
-mat=[[1,0,1,1],[1,0,1,1],[1,1,0,0],[1,1,1,1]]
+mat = []
+mat = [[1, 0, 1, 1], [1, 0, 1, 1], [1, 1, 0, 0], [1, 1, 1, 1]]
 
-# row parsing list
-row_parse=[]
-row_parse=[0, 0, 1, 1]
+row_parse = [0, 0, 1, 1]
 
-#column parsing list
-col_parse=[]
-col_parse=[1, 1, 0, 0]
+col_parse = [1, 1, 0, 0]
 
-#modified mat with error is
-modified_mat=[[1,0,1,1],[1,1,1,1],[1,1,0,0],[1,1,1,1]]
+modified_mat = [[1, 0, 1, 1], [1, 1, 1, 1], [1, 1, 0, 0], [1, 1, 1, 1]]
 
-#sending the modified matrix
-# data1=str(modified_mat)
-# data1=data1.encode()
-data1=pickle.dumps(modified_mat)
-s.send(data1)
-
-#adding 1 sec time for correct execution
+data1 = pickle.dumps(modified_mat)
+sock_client.send(data1)
 time.sleep(1)
 
-#sending the row parsing list
-# data2=str(row_parse)
-# data2=data2.encode()
-data2=pickle.dumps(row_parse)
-s.send(data2)
-
-#adding 1 sec time for correct execution
+data2 = pickle.dumps(row_parse)
+sock_client.send(data2)
 time.sleep(1)
 
-#sending the column parsing list
-# data3=str(col_parse)
-# data3=data3.encode()
-data3=pickle.dumps(col_parse)
-s.send(data3)
+data3 = pickle.dumps(col_parse)
+sock_client.send(data3)
+time.sleep(1)
 
-matrix=pickle.loads(s.recv(1024))
+matrix = pickle.loads(sock_client.recv(1024))
 print("The recovered matrix is:")
 print(matrix)
 
-
-
-s.close() 
+sock_client.close() 
--- Lab3/server.py
+++ Lab3/server.py
@@ -14,22 +14,23 @@ print("The received matrix is:")
 print(matrix_received)
 
 row_parity_original = pickle.loads(sock_client.recv(1024))
+print("Row parity bits:")
 print(row_parity_original)
 
 column_parity_original = pickle.loads(sock_client.recv(1024))
+print("Column parity bits:")
 print(column_parity_original)
 
 row_parity_calculated = []
 col_parity_calculated = []
 
-#Finding parity row&column
 for i in range(len(matrix_received)):
     count = 0
     for j in range(len(matrix_received[i])):
         if(matrix_received[i][j] == 1):
-            count = count+1
+            count = count + 1
 
-    if(count%2 != 0):
+    if(count % 2 != 0):
         row_parity_calculated.append(0)
     else:
         row_parity_calculated.append(1)
@@ -38,17 +39,13 @@ for i in range(len(matrix_received)):
     count = 0
     for j in range(len(matrix_received[i])):
         if(matrix_received[j][i] == 1):
-            count = count+1
+            count = count + 1
 
-    if(count%2 != 0):
+    if(count % 2 != 0):
         col_parity_calculated.append(0)
     else:
         col_parity_calculated.append(1)   
 
-# print(row_parity)
-# print(col_parity)
-
-#Error Detection
 row_error = 0
 col_error = 0
 l = 0
@@ -56,29 +53,22 @@ m = 0
 for k in range(len(row_parity_calculated)):
     if(row_parity_calculated[l] != row_parity_original[l]):
        row_error = l
-    l = l+1   
+    l = l + 1   
 for k in range(len(col_parity_calculated)):
     if(col_parity_calculated[m] != column_parity_original[m]):
        col_error = m
-    m = m+1
+    m = m + 1
 
-# print(row_error)
-# print(col_error) 
-
-#Error Correction
-cor_mat = []
-cor_mat = matrix_received
-if(cor_mat[row_error][col_error] == 1):
-    cor_mat[row_error][col_error] = 0
+corrected_matrix = matrix_received
+if(corrected_matrix[row_error][col_error] == 1):
+    corrected_matrix[row_error][col_error] = 0
 else:
-    cor_mat[row_error][col_error] = 1
+    corrected_matrix[row_error][col_error] = 1
 
-print("The Recovered Matrix is :")
-print(cor_mat)
+print("Corrected matrix:")
+print(corrected_matrix)
 
-
-matrix = pickle.dumps(cor_mat)
+matrix = pickle.dumps(corrected_matrix)
 sock_client.send(matrix)
-
 
 sock_client.close()
